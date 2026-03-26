6c32c5275c2b97caf66235d86a7d297e63fba767
psicjesusmontanez-oss <psic.jesusmontanez@gmail.com>
2026-03-26 17:20:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # --- CONFIGURACIÓN DE LA PÁGINA WEB ---
-st.set_page_config(page_title="Índice VFC", layout="wide")
+st.set_page_config(page_title="Diagnóstico A.R.M. | VFC", layout="wide")
 st.title("Arquitectura de Resiliencia Multidimensional (A.R.M.)")
-st.subheader("Plataforma de Diagnóstico Biométrico")
+st.subheader("Plataforma de Diagnóstico Autonómico y Neuroquímico")
 
 # --- PROTOCOLOS CLÍNICOS ---
 PROTOCOLOS = {
